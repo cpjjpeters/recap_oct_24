8250a0899c657979e706cec1e3b1d8ac94738b8e
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-10-18 18:32:24 +0200

    added new table

---
+++ src/main/java/be/ipeters/recap/model/IssuerPricingAudit.java
@@ -0,0 +1,27 @@
+package be.ipeters.recap.model;
+
+/* carlpeters created on 18/10/2024 inside the package - be.ipeters.recap.model */
+public class IssuerPricingAudit {
+    private String id;
+    private String costsLinkedToInstrument;
+    private String costsLinkedToService;
+    private String issueProgramIdentifier;
+    private String issuePIdentifier;
+    private String organisationIdentifier;
+    private String organisationType;
+    private String previousId;
+    private String referenceRate;
+    private String referenceRateOther;
+    private String spreadValue;
+    private String tenor;
+    private String tenorUnit;
+    private String tenorValue;
+    private String updateIdentifier;
+    private String updateTimestamp;
+    private String updateType;
+    private String userIdentifier;
+    private String yieldIssuer;
+    private String yieldType;
+
+
+}
