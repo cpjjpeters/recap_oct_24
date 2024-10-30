07ffd3adfa53546483ca72a3306c4901306ea634
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-10-30 10:26:38 +0100

    unnecessary 2

---
+++ src/main/java/be/ipeters/recap/unnecessary/DoesNothing2.java
@@ -0,0 +1,8 @@
+package be.ipeters.recap.unnecessary;
+
+/* carlpeters created on 30/10/2024 inside the package - be.ipeters.recap.unnecessary */
+public class DoesNothing2 {
+    public void doesNothing2() {
+        System.out.println("Does nothing2");
+    }
+}
